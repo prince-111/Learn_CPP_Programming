d6776590cedca9aba493ed3124499bb1b72ad051
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-09 04:48:16 +0000

    🥴 you are adult or not.

---
+++ Basic of C++/if_else.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+// write a program that takes an input of age
+// and prints if you are adult or not
+// >=18, yes
+// < 18, no
+int main() {
+	int age;
+	cin>>age;
+	
+	if(age >=18){
+	    cout<<"You are an adult !";
+	}
+	
+	else{
+	   cout<<"You are not an adult !";    
+	}
+	return 0;
+}
