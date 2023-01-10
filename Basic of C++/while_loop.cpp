c49b47bfe98c232b9f7a469c5a080da470fa4633
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-10 12:57:31 +0000

    ➰ sum of natural number

---
+++ Basic of C++/while_loop.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){ 
+    /*
+       while (condition) {
+         // body of the loop
+       }
+    */
+    
+    int i=1;
+    while(i<=5){
+        cout<<"Prince"<<endl;
+        
+        i= i+1;
+    }
+    
+    // print the number
+    int t=1;
+    while(t<=5){
+        cout<<t<<" ";
+        t++;
+    }
+    
+    //sum the number
+    int k=1, sum =0;
+    while(k<=5){
+        sum += k;
+        
+        k++;
+    }
+    cout<<"Sum = "<<sum;
+    
+    
+  
+	return 0;
+}
