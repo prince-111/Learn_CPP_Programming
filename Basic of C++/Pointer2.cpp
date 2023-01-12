3d1e0aeefba52ca155ee3c84ac732e3b6f7f2978
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-12 12:14:41 +0000

    ⚡ passing by refferrence with or without

---
+++ Basic of C++/Pointer2.cpp
@@ -0,0 +1,64 @@
+// Example 1: Passing by reference without pointers
+#include <iostream>
+using namespace std;
+// function definition to swap values
+void swap(int &n1, int &n2) {
+    int temp;
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+int main()
+{
+
+    // initialize variables
+    int a = 1, b = 2;
+
+    cout << "Before swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    // call function to swap numbers
+    swap(a, b);
+
+    cout << "\nAfter swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
+
+// Example 2: Passing by reference using pointers
+#include <iostream>
+using namespace std;
+
+// function prototype with pointer as parameters
+void swap(int*, int*);
+
+int main()
+{
+
+    // initialize variables
+    int a = 1, b = 2;
+
+    cout << "Before swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    // call function by passing variable addresses
+    swap(&a, &b);
+
+    cout << "\nAfter swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
+
+// function definition to swap numbers
+void swap(int* n1, int* n2) {
+    int temp;
+    temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
