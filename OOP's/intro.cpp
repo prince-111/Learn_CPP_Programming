d60481076fb6a736c8e2a59b3c323141d81adf6b
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-02-14 17:32:31 +0000

    create a class & objects 🌱

---
+++ OOP's/intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// class
+class hero{
+
+    // properties
+    int health;
+};
+
+int main(){
+	
+	// creation an Object of class;
+	hero ram;
+    
+	cout<<"size:-"<<sizeof(ram)<<endl;	
+}
